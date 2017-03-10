a3a4cdecfdbbe2da8a418fc1bbe48d8aedc85631
Yin Zhuochao <yinzhuochao@126.com>
2017-03-10 18:00:42 +0800

    repair

--- finished/receipt_management.go
+++ finished/receipt_management.go
@@ -11,17 +11,17 @@ import (
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"github.com/hyperledger/fabric/core/crypto/primitives"
-	//"github.com/op/go-logging"
+	"github.com/op/go-logging"
 )
 
-//var myLogger = logging.MustGetLogger("asset_mgm")
+var myLogger = logging.MustGetLogger("asset_mgm")
 
 type AssetManagementChaincode struct {
 }
 
 // The deploy transaction metadata is supposed to contain the warehouse cert
 func (t *AssetManagementChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	//myLogger.Debug("Init Chaincode...")
+	myLogger.Debug("Init Chaincode...")
 	if len(args) != 0 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 0")
 	}
@@ -40,7 +40,7 @@ func (t *AssetManagementChaincode) Init(stub shim.ChaincodeStubInterface, functi
 }
 
 func (t *AssetManagementChaincode) setCert(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	//myLogger.Debug("Set Cert...")
+	myLogger.Debug("Set Cert...")
 	if len(args) != 2 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 2")
 	}
@@ -51,7 +51,7 @@ func (t *AssetManagementChaincode) setCert(stub shim.ChaincodeStubInterface, arg
 		return nil, errors.New("Failed decoding warehouseCert")
 	}
 
-	//myLogger.Debugf("The warehouse is [% x]", warehouseCert)
+	myLogger.Debugf("The warehouse is [% x]", warehouseCert)
 
 	stub.PutState("warehouse", warehouseCert)
 	
@@ -61,17 +61,17 @@ func (t *AssetManagementChaincode) setCert(stub shim.ChaincodeStubInterface, arg
 		return nil, errors.New("Failed decoding warehouseCert2")
 	}
 
-	//myLogger.Debugf("The warehouse2 is [% x]", warehouseCert2)
+	myLogger.Debugf("The warehouse2 is [% x]", warehouseCert2)
 
 	stub.PutState("warehouse2", warehouseCert2)
 
-	//myLogger.Debug("Set Cert...done")
+	myLogger.Debug("Set Cert...done")
 
 	return nil, nil
 }
 
 func (t *AssetManagementChaincode) assign(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	//myLogger.Debug("Assign...")
+	myLogger.Debug("Assign...")
 
 	if len(args) != 4 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 4")
@@ -101,7 +101,7 @@ func (t *AssetManagementChaincode) assign(stub shim.ChaincodeStubInterface, args
 	}
 
 	// Register assignment
-	//myLogger.Debugf("New owner of [%s] is [% x]", receiptId, owner)
+	myLogger.Debugf("New owner of [%s] is [% x]", receiptId, owner)
 
 	ok, err = stub.InsertRow("AssetsOwnership", shim.Row{
 		Columns: []*shim.Column{
@@ -114,13 +114,13 @@ func (t *AssetManagementChaincode) assign(stub shim.ChaincodeStubInterface, args
 		return nil, errors.New("Receipt has already existed.")
 	}
 
-	//myLogger.Debug("Assign...done!")
+	myLogger.Debug("Assign...done!")
 
 	return nil, err
 }
 
 func (t *AssetManagementChaincode) transfer(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	//myLogger.Debug("Transfer...")
+	myLogger.Debug("Transfer...")
 
 	if len(args) != 3 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 3")
@@ -145,7 +145,7 @@ func (t *AssetManagementChaincode) transfer(stub shim.ChaincodeStubInterface, ar
 	}
 
 	prvOwner := row.Columns[2].GetBytes()
-	//myLogger.Debugf("Previous owener of [%s] is [% x]", asset, prvOwner)
+	myLogger.Debugf("Previous owener of [%s] is [% x]", asset, prvOwner)
 	if len(prvOwner) == 0 {
 		return nil, fmt.Errorf("Invalid previous owner. Nil")
 	}
@@ -186,15 +186,15 @@ func (t *AssetManagementChaincode) transfer(stub shim.ChaincodeStubInterface, ar
         return nil, errors.New("Failed setting event.")
     }
 
-	//myLogger.Debugf("New owner of [%s] is [% x]", asset, newOwner)
+	myLogger.Debugf("New owner of [%s] is [% x]", asset, newOwner)
 
-	//myLogger.Debug("Transfer...done")
+	myLogger.Debug("Transfer...done")
 
 	return nil, nil
 }
 
 func (t *AssetManagementChaincode) isCaller(stub shim.ChaincodeStubInterface, certificate []byte) (bool, error) {
-	//myLogger.Debug("Check caller...")
+	myLogger.Debug("Check caller...")
 
 	// In order to enforce access control, we require that the
 	// metadata contains the signature under the signing key corresponding
@@ -218,10 +218,10 @@ func (t *AssetManagementChaincode) isCaller(stub shim.ChaincodeStubInterface, ce
 		return false, errors.New("Failed getting binding")
 	}
 
-	//myLogger.Debugf("passed certificate [% x]", certificate)
-	//myLogger.Debugf("passed sigma [% x]", sigma)
-	//myLogger.Debugf("passed payload [% x]", payload)
-	//myLogger.Debugf("passed binding [% x]", binding)
+	myLogger.Debugf("passed certificate [% x]", certificate)
+	myLogger.Debugf("passed sigma [% x]", sigma)
+	myLogger.Debugf("passed payload [% x]", payload)
+	myLogger.Debugf("passed binding [% x]", binding)
 
 	ok, err := stub.VerifySignature(
 		certificate,
@@ -229,15 +229,15 @@ func (t *AssetManagementChaincode) isCaller(stub shim.ChaincodeStubInterface, ce
 		append(payload, binding...),
 	)
 	if err != nil {
-		//myLogger.Errorf("Failed checking signature [%s]", err)
+		myLogger.Errorf("Failed checking signature [%s]", err)
 		return ok, err
 	}
 	if !ok {
-		//myLogger.Error("Invalid signature!!!")
+		myLogger.Error("Invalid signature!!!")
 		return ok, err
 	}
 
-	//myLogger.Debug("Check caller...Verified!")
+	myLogger.Debug("Check caller...Verified!")
 
 	return ok, err
 }
@@ -271,7 +271,7 @@ func (t *AssetManagementChaincode) Invoke(stub shim.ChaincodeStubInterface, func
 // "query(asset)": returns the owner of the asset.
 // Anyone can invoke this function.
 func (t *AssetManagementChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	//myLogger.Debugf("Query [%s]", function)
+	myLogger.Debugf("Query [%s]", function)
 
 	if function != "query" {
 		return nil, errors.New("Invalid query function name. Expecting 'query' but found '" + function + "'")
@@ -280,14 +280,14 @@ func (t *AssetManagementChaincode) Query(stub shim.ChaincodeStubInterface, funct
 	var err error
 
 	if len(args) != 2 {
-		//myLogger.Debug("Incorrect number of arguments. Expecting 2")
+		myLogger.Debug("Incorrect number of arguments. Expecting 2")
 		return nil, errors.New("Incorrect number of arguments. Expecting 2")
 	}
 
 	// Who is the owner of the asset?
 	asset := args[1]
 
-	//myLogger.Debugf("Arg [%s]", string(asset))
+	myLogger.Debugf("Arg [%s]", string(asset))
 
 	var columns []shim.Column
 	col1 := shim.Column{Value: &shim.Column_String_{String_: asset}}
@@ -295,22 +295,22 @@ func (t *AssetManagementChaincode) Query(stub shim.ChaincodeStubInterface, funct
 
 	row, err := stub.GetRow("AssetsOwnership", columns)
 	if err != nil {
-		//myLogger.Debugf("Failed retriving receiptId [%s]: [%s]", string(asset), err)
+		myLogger.Debugf("Failed retriving receiptId [%s]: [%s]", string(asset), err)
 		return nil, fmt.Errorf("Failed retriving receiptId [%s]: [%s]", string(asset), err)
 	}
 
 	if len(row.Columns)==0 {
-	    //myLogger.Debugf("No row in result set for receiptId=%s", string(asset))
+	    myLogger.Debugf("No row in result set for receiptId=%s", string(asset))
 		return nil, fmt.Errorf("No row in result set for receiptId=%s", string(asset))
 	}
 
-	//myLogger.Debugf("row=", row)
+	myLogger.Debugf("row=", row)
 
     if args[0] == "getOwner" {
-    	//myLogger.Debugf("Query done. row.Columns[2]=[% x]", row.Columns[2].GetBytes())
+    	myLogger.Debugf("Query done. row.Columns[2]=[% x]", row.Columns[2].GetBytes())
 	    return row.Columns[2].GetBytes(), nil
     }else{
-    	//myLogger.Debugf("Query done. row.Columns[1]=[%s]", row.Columns[1])
+    	myLogger.Debugf("Query done. row.Columns[1]=[%s]", row.Columns[1])
     	return []byte(row.Columns[1].GetString_()), nil
     }
 }
