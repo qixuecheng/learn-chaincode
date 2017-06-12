4ea3906a4b04ccd5358174a45deed0aa74af7201
stephem <529489422@qq.com>
2017-06-12 23:43:13 +0800

    666

--- finished/test.go
+++ finished/test.go
@@ -111,31 +111,25 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
-
 	return valAsbytes, nil
 }
 func (t *SimpleChaincode) readones(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var Resp string
+	Resp := []byte("")
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 	}
 	key := args[0]
 	params := strings.Split(key, ",")
-	for _,x:= range params {
-		valAsbytes, err := stub.GetState(x)
+	for i,_:= range params {
+		valAsbytes, err := stub.GetState(params[i])
 		if err != nil {
 			Resp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 			return nil, errors.New(Resp)
 		}
-		Resp = Resp + convert(valAsbytes)+","
+		Resp = BytesCombine(Resp,valAsbytes)
 	}
-	Respbytes :=[]byte(Resp)
-	return Respbytes, nil
+	return Resp, nil
 }
-func convert(b []byte ) string {
-	s := make([]string,len(b))
-	for i := range b {
-		s[i] = strconv.Itoa(int(b[i]))
-	}
-	return strings.Join(s,",")
+func BytesCombine(pBytes ...[]byte) []byte {
+	return bytes.Join(pBytes, []byte(","))
 }
